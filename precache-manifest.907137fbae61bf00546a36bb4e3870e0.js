468652d779e8b01882e19a92167a2bd90287e03f
Michael Hernandez <michael.hernandez27@outlook.com>
2020-07-01 19:08:17 -0400

    Updates

---
+++ precache-manifest.907137fbae61bf00546a36bb4e3870e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa794b6b153c38f0ab448ab70a03f516",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "3b3c7feb9e6208ddc35b",
+    "url": "/Clicky-Game/static/css/main.a19f5b0e.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3c7feb9e6208ddc35b",
+    "url": "/Clicky-Game/static/js/main.06fee9bf.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  }
+]);
